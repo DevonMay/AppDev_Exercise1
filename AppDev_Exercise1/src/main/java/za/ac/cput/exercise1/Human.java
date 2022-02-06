71ab691c995dc499ea04637fc6406d516a0db579
Cameron Noemdo <219115443@mycput.ac.za>
2022-02-06 23:05:55 +0200

    Added to string for human class

--- AppDev_Exercise1/src/main/java/za/ac/cput/exercise1/Human.java
+++ AppDev_Exercise1/src/main/java/za/ac/cput/exercise1/Human.java
@@ -24,4 +24,12 @@ public class Human {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "firstName='" + firstName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
