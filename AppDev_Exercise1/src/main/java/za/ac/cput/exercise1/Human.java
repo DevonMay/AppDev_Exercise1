d54383f49b5ecd754b7636cd359adbdb34d6e78e
Cameron Noemdo <219115443@mycput.ac.za>
2022-02-06 22:48:12 +0200

    Added human class

---
+++ AppDev_Exercise1/src/main/java/za/ac/cput/exercise1/Human.java
@@ -0,0 +1,27 @@
+package za.ac.cput.exercise1;
+
+public class Human {
+    private String firstName;
+    private byte age;
+
+    public Human(String firstName, byte age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+}
