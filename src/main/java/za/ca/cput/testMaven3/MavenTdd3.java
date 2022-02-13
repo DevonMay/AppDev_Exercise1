648d78169d8ff519f20e65c16b087afb227f194c
TaariqKhan <taariqkhs@gmail.com>
2022-02-13 16:48:37 +0200

    final commit

---
+++ src/main/java/za/ca/cput/testMaven3/MavenTdd3.java
@@ -0,0 +1,39 @@
+package za.ca.cput.testMaven3;
+
+public class MavenTdd3
+{
+    private int tmt1;
+    private int tmt2;
+
+    public MavenTdd3()
+    {
+        this.tmt1 = tmt1;
+        this.tmt2 = tmt2;
+    }
+
+    public int getTmt1()
+    {
+        return tmt1;
+    }
+
+    public void setTmt1(int tmt1)
+    {
+        this.tmt1 = tmt1;
+    }
+
+    public int getTmt2()
+    {
+        return tmt2;
+    }
+
+    public void setTmt2(int tmt2)
+    {
+        this.tmt2 = tmt2;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "MavenTdd3{" + "tmt1=" + tmt1 + ", tmt2=" + tmt2 + '}';
+    }
+}
